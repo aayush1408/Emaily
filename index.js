dda533056740f3a6b28ffcdfb2cf2e1faeb6d423
aayush1408 <aayushjaiswal0114@gmail.com>
2018-01-07 16:28:39 +0530

    oauth completed

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+require('./models/User.js');
+
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+require('./services/passport.js');
+const mongoose = require('mongoose');
+const keys = require('./config/keys.js');
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('Hello World');
-});
+app.use(cookieSession({
+    maxAge:30*24*60*60*1000,
+    keys:[keys.cookieKey]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes.js')(app);
+
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
